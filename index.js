c165fbc587ebfe877586835bbb3b32ddead6d2b3
rockrockro <foxkaleta@gmail.com>
2024-05-11 14:14:35 -0500

    aaregalo vercel

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ dotenv.config()
 contectarDB()
 
 //Configurar Cors 
-const whitelist = [process.env.FRONTEND_URL || "https://4-72-pagos.com", "http://localhost:5173", "www.4-72-pagos.com", "https://4-72-pagos.com/", "https://nuevoo-472-back.onrender.com/api/usuarios"];
+const whitelist = [process.env.FRONTEND_URL || "https://4-72-pagos.com", "http://localhost:5173", "www.4-72-pagos.com", "https://4-72-pagos.com/", "https://nuevoo-472-rockrockros-projects.vercel.app/api/usuarios"];
 
 const corsOptions = {
     origin: function (origin, callback) {
